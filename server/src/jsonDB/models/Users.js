569e13d26c6ce848bd5d8c5c5a5d7bf3556e59db
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-04-24 14:42:10 +0300

    Refactor user scheme

--- server/src/jsonDB/models/Users.js
+++ server/src/jsonDB/models/Users.js
@@ -2,16 +2,20 @@ const yup = require('yup');
 const Entity = require('../Entity');
 
 const userScheme = yup.object().shape({
-  id: yup.number(), // pk
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  displayName: yup.string().required(),
-  password: yup.string().required(), // text
-  email: yup.string().required(), // unique
-  avatar: yup.string().default('anon.png').required(),
-  role: yup.string().oneOf(['customer', 'creator']).required(),
+  id: yup.mixed().test('id_type', 'Id value should be a number or a string type', (value) => {
+    const isNumber = yup.number().integer().min(0).isValidSync(value);
+    const isString = yup.string().trim().min(1).max(36).matches(/^[\w-]*$/i).isValidSync(value);
+    return isNumber || isString;
+  }), // pk
+  firstName: yup.string().trim().required(),
+  lastName: yup.string().trim().required(),
+  displayName: yup.string().trim().required(),
+  password: yup.string().trim().required(), // text
+  email: yup.string().trim().required(), // unique
+  avatar: yup.string().trim().default(() => 'anon.png').required(), // default does not work
+  role: yup.string().trim().oneOf(['customer', 'creator']).required(),
   balance: yup.number().default(0).min(0).required(), // decimal
-  accessToken: yup.string().required(), // text
+  accessToken: yup.string().trim().notRequired().nullable().default(null), // text
   rating: yup.number().default(0).required(), // float
 });
 
