c1dfe4317916686bb72ed7f03728ab84a2ee1e4c
Viktoriia <pecourses>
2022-07-08 01:57:20 +0300

    Delete saga mw from store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,18 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducer';
-import rootSaga from '../sagas/rootSaga';
 import { initSocket } from '../api/ws/socketController';
-
-const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(sagaMiddleware),
 });
-
-sagaMiddleware.run(rootSaga, store.dispatch);
 
 initSocket(store);
 
