d9cad620402ca67cbde958f1446804b77502b5c2
Viktoriia <pecourses>
2022-09-21 01:52:35 +0300

    Rename reducers

--- client/src/app/reducer.js
+++ client/src/app/reducer.js
@@ -1,26 +1,26 @@
 import { combineReducers } from 'redux';
 import authReducer from './slices/authSlice';
-import getUserReducer from './slices/userSlice';
+import userReducer from './slices/userSlice';
 import dataForContestReducer from './slices/dataForContestSlice';
 import paymentReducer from './slices/paymentSlice';
-import getContestsReducer from './slices/contestsSlice';
-import storeContestReducer from './slices/contestSavingSlice';
+import contestsReducer from './slices/contestsSlice';
+import contestSavingReducer from './slices/contestSavingSlice';
 import bundleReducer from './slices/bundleSlice';
-import getContestByIdReducer from './slices/contestByIdSlice';
-import updateContestReducer from './slices/contestUpdationSlice';
+import contestByIdReducer from './slices/contestByIdSlice';
+import contestUpdationReducer from './slices/contestUpdationSlice';
 import chatReducer from './slices/chatSlice';
 import userProfileReducer from './slices/userProfileSlice';
 
 const appReducer = combineReducers({
-  userStore: getUserReducer,
+  userStore: userReducer,
   auth: authReducer,
   dataForContest: dataForContestReducer,
   payment: paymentReducer,
-  contestByIdStore: getContestByIdReducer,
-  contestsList: getContestsReducer,
-  contestStore: storeContestReducer,
+  contestByIdStore: contestByIdReducer,
+  contestsList: contestsReducer,
+  contestStore: contestSavingReducer,
   bundleStore: bundleReducer,
-  updateContestStore: updateContestReducer,
+  updateContestStore: contestUpdationReducer,
   chatStore: chatReducer,
   userProfile: userProfileReducer,
 });
